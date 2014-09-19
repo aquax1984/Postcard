317a915bad2ec24b9739e987a2db30f7bf7e924f
Chris Bouquet <Chris@Chris-Bouquets-MacBook-Pro-2.local>
2014-09-18 19:52:28 -0600

    second commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         
         //code will evaulate when we press the button
+        //adding a comment to test commits
+    
         messageLabel.hidden = false
         
         messageLabel.text = enterMessageTextField.text
